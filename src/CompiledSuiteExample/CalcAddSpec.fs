ef4ebea23cb1b00070157601af4c1919391ded81
Mike Chaliy <mike@chaliy.name>
2010-02-20 02:26:18 +0200

    Added ability to use non spec lest in spec modules

--- src/CompiledSuiteExample/CalcAddSpec.fs
+++ src/CompiledSuiteExample/CalcAddSpec.fs
@@ -1,14 +1,21 @@
 ﻿module ``Calculator Add Operation Specification``
 open FsSpec
 
-module ``Describe simple add cases`` = 
+module ``Describe simple add cases`` =     
+
+    let res = Calc.Add 2 2
 
     let ``add two integers`` = spec {
-        let res = Calc.Add 2 2
         res.should_be_equal_to 4 
     }
 
-    let ``add two integers (Unexpected Result)`` = spec {       
-        let res = Calc.Add 2 2
+    let ``add two integers (Unexpected Result)`` = spec {        
         res.should_be_equal_to 5
     }   
+
+module ``Describe advanced add cases`` =         
+
+    let ``add two positive and negative`` = spec {
+        let res = Calc.Add 10 -10
+        res.should_be_equal_to 0
+    }    
--- src/FsSpec/FsSpec.fs
+++ src/FsSpec/FsSpec.fs
@@ -2,50 +2,6 @@
 
 exception AssertFailed of string
 
-module Runner =
-    open System.Reflection
-    //open Microsoft.FSharp.Reflection
-    open Microsoft.FSharp.Metadata
-
-    let internal printTitle (title : string) = 
-            let sep = (new System.String('-', title.Length))
-            printfn "\r\n%s\r\n%s\r\n%s\r\n"  sep title sep 
-
-    let Run() =
-        let runTest (mem : FSharpMemberOrVal) =
-            try                
-                let spec = match mem.ReflectionMemberInfo with
-                           | :? PropertyInfo as p -> p.GetValue(null, null)  :?> (unit -> unit)
-                           | :? MethodInfo as m -> m.Invoke(null, null)  :?> (unit -> unit)
-                           | _ -> failwith "Spec definition is unsupported" // TODO: Explain this
-                
-                spec()
-                printfn "OK"
-            with
-                | AssertFailed x -> printfn "%s" x                
-                | x -> printfn "%s\r\n%s" x.Message (x.ToString())                
-        
-
-        let asm = System.Reflection.Assembly.GetEntryAssembly()                    
-        let fasm = FSharpAssembly.FromAssembly(asm)
-        let ee = fasm.Entities
-                    |> Seq.filter(fun e -> e.IsModule)
-                    |> Seq.filter(fun e -> e.LogicalName.Contains("Spec"))                    
-                    |> Seq.filter(fun e -> ( printTitle e.CompiledName ; true ) )
-                    |> Seq.collect(fun e -> e.NestedEntities)
-                    |> Seq.filter(fun e -> e.IsModule)
-                    |> Seq.filter(fun e -> e.LogicalName.Contains("Describe"))
-                    |> Seq.filter(fun e -> ( printfn "\t%s\r\n" e.CompiledName ; true ) )
-                    |> Seq.collect(fun e -> e.MembersOrValues)
-                    // TODO: Should filter out unneeded stuff...
-                    |> Seq.filter(fun m -> ( printf "\t\t- Should %s - " m.CompiledName ; true ) )
-                    |> Seq.iter(runTest)
-                    //|> Seq.filter(fun m -> m.Type = (unit -> unit))
-                    //|> Seq.toArray                
-
-        ()
-
-
 [<AutoOpen>]
 module SpecHelpers =
            
@@ -62,18 +18,14 @@ module SpecHelpers =
             | AssertFailed x -> printfn "\t\t- %s - %s" desc x
             | x -> printfn "\t\t- %s - %s\r\n%s" desc x.Message (x.ToString())               
 
-    // Builder
+    // Spec Builder
 
-    type Spec = (unit -> unit)
-
-    let runSpec (a:Spec) = a() 
-    let delay f = (fun() -> runSpec (f()))
-
+    type Spec = unit -> unit    
      
     type SpecBuilder() =
-        
-        member b.Delay(f) = delay f
-        member b.Zero() = (fun uint -> ())
+                
+        member b.Delay(f : unit -> Spec) = (fun() -> f()())
+        member b.Zero() = (fun() -> ())
 
     let spec = new SpecBuilder()
 
@@ -98,3 +50,44 @@ module SpecHelpers =
 //let throws_exception() : unit =
 //  raise(System.ArgumentException "Bad things")
 // http://weblogs.asp.net/podwysocki/archive/2008/06/04/language-oriented-programming-and-functional-unit-testing-in-f.aspx
+
+module Runner =
+    open System.Reflection    
+    open Microsoft.FSharp.Metadata
+
+    let internal printTitle (title : string) = 
+            let sep = (new System.String('-', title.Length + 4))
+            printfn "\r\n%s\r\n  %s  \r\n%s\r\n"  sep title sep 
+
+    let Run() =
+        let runTest (spec : Spec) =
+            try                                
+                spec()
+                printfn "OK"
+            with
+                | AssertFailed x -> printfn "%s" x                
+                | x -> printfn "%s\r\n%s" x.Message (x.ToString())                
+        
+
+        let asm = System.Reflection.Assembly.GetEntryAssembly()                    
+        let fasm = FSharpAssembly.FromAssembly(asm)
+        fasm.Entities
+                    |> Seq.filter(fun e -> e.IsModule)
+                    |> Seq.filter(fun e -> e.LogicalName.Contains("Spec"))                    
+                    |> Seq.filter(fun e -> ( printTitle e.CompiledName ; true ) )
+                    |> Seq.collect(fun e -> e.NestedEntities)
+                    |> Seq.filter(fun e -> e.IsModule)
+                    |> Seq.filter(fun e -> e.LogicalName.Contains("Describe"))
+                    |> Seq.filter(fun e -> ( printfn "\r\n\t%s\r\n" e.CompiledName ; true ) )
+                    |> Seq.collect(fun e -> e.MembersOrValues) 
+                    |> Seq.map(fun m -> (m, (match m.ReflectionMemberInfo with
+                                               | :? PropertyInfo as p -> Some(p.GetValue(null, null))
+                                               | :? MethodInfo as m -> Some(m.Invoke(null, null))
+                                               | _ -> None)))                    
+                    |> Seq.filter(fun (_ , s) -> s.IsSome) 
+                    |> Seq.map(fun (m, s) -> (m, (match s.Value with
+                                                    | :? Spec as spec -> Some(spec)
+                                                    | _ -> None )))
+                    |> Seq.filter(fun (_ , s) -> s.IsSome) 
+                    |> Seq.filter(fun (m, _) -> ( printf "\t\t- Should %s - " m.CompiledName ; true ) )
+                    |> Seq.iter(fun (_, s) -> runTest s.Value)         
